7a5e5c6f75b8f1256aa8f9a59fdbcb54ef7c52f8
Abhishek-M-K2005 <amkarennavar@gmail.com>
2025-10-06 22:56:06 +0530

    Additional Changes in test.py

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ from sklearn.neighbors import KNeighborsClassifier
 import pickle
 import numpy as np
 import os
+import csv
+import time
+from datetime import datetime 
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier("./data/haarcascade_frontalface_default.xml")
 
@@ -15,6 +18,9 @@ with open("data/face_data.pkl") as f:
 knn = KNeighborsClassifier(n_neighbors = 5)
 knn.fit(X, Y)
 
+COL_NAMES =["Name", "Time"]
+
+
 while True:
     ret, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -23,7 +29,12 @@ while True:
         crp_img = frame[y : y+h, x : x + w, :]
         rsz_img = cv2.resize(crp_img, (50, 50))
         output = knn.predict(rsz_img)
+        ts = time.time()
+        date = datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        ts = datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+        
         cv2.putText(frame, str(output), (x, y - 15), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y - 40), (0, 0, 255), 5)
         cv2.rectangle(frame, (x, y), (x+ w, y + h),(50, 50, 255), 5)
     cv2.imshow("frame", frame)
     k = cv2.waitKey(1)
